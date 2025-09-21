efc034f55608f308f1d53c2c7b40b49e520ab81a
Akshay Raj P <akshay_b220666pe@nitc.ac.in>
2025-09-21 10:03:31 +0530

    learning

--- ML.py
+++ ML.py
@@ -2,3 +2,11 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
+
+train_url = 'https://raw.githubusercontent.com/akshayraj0674/MachineLearning/refs/heads/main/project-1-me-4127-e-2025-26/train.csv'
+test_url = 'https://raw.githubusercontent.com/akshayraj0674/MachineLearning/refs/heads/main/project-1-me-4127-e-2025-26/test.csv'
+sample_submission_url = 'https://raw.githubusercontent.com/akshayraj0674/MachineLearning/refs/heads/main/project-1-me-4127-e-2025-26/sample_submission.csv'
+
+train = pd.read_csv(train_url)
+test = pd.read_csv(test_url)
+sample_submission = pd.read_csv(sample_submission_url)
