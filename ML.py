70994a49353dfd3ec4efca15595794bbb7f7ba97
Akshay Raj P <akshay_b220666pe@nitc.ac.in>
2025-09-27 23:43:31 +0530

    learning

--- ML.py
+++ ML.py
@@ -1,7 +1,9 @@
+import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVR
+from scipy.stats import boxcox
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 
 
 train_url = 'https://raw.githubusercontent.com/akshayraj0674/MachineLearning/refs/heads/main/project-1-me-4127-e-2025-26/train.csv'
@@ -18,25 +20,51 @@ print(test.head())
 print(sample_submission.head())
 
 
-x_train = train.drop('cost', axis=1)
-y_train = train['cost']
+target = 'cost'
+
+features = [col for col in train.columns if col not in [target, 'id']]
+
+x = train[features].values
+y = train[target].values
 
 
-x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.2, random_state=42)
+if np.any(y <= 0):
+    shift = np.abs(np.min(y)) + 1
+    y_bc, bc_lambda = boxcox(y + shift)
+else:
+    shift = 0
+    y_bc, bc_lambda = boxcox(y)
 
 
-model = SVR(kernel='linear', C=1.0, gamma='scale')
-model.fit(x_train, y_train)
+x_train, x_val, y_train_bc, y_val_bc = train_test_split(x, y_bc, test_size=0.2, random_state=42)
 
 
-y_val_pred = model.predict(x_val)
-mse = mean_squared_error(y_val, y_val_pred)
-print("Validation Mean Squared Error:", mse)
+lr = LinearRegression()
+lr.fit(x_train, y_train_bc)
 
 
-test_pred = model.predict(test)
+y_val_pred_bc = lr.predict(x_val)
+
+
+def inv_boxcox(y, lmbda, shift):
+    if lmbda == 0:
+        return np.exp(y) - shift
+    else:
+        return np.power(y * lmbda + 1, 1 / lmbda) - shift
+
+
+y_value_pred = inv_boxcox(y_val_pred_bc, bc_lambda, shift)
+y_value_true = inv_boxcox(y_val_bc, bc_lambda, shift)
+
+
+lr.fit(x, y_bc)
+
+
+x_test = test[features].values
+y_test_pred_bc = lr.predict(x_test)
+y_test_pred = inv_boxcox(y_test_pred_bc, bc_lambda, shift)
 
 
 submission = sample_submission.copy()
-submission['cost'] = test_pred
+submission[target] = y_test_pred
 submission.to_csv('submission.csv', index=False)
