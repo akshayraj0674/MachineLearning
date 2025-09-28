7e506fc7b4f74c5e65b5097a8ed39c34235efe79
Akshay Raj P <akshay_b220666pe@nitc.ac.in>
2025-09-28 18:06:14 +0530

    learning

--- ML.py
+++ ML.py
@@ -1,8 +1,8 @@
 import pandas as pd
-from sklearn.linear_model import HuberRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.impute import SimpleImputer
 
+from ML import x_train
 
 train_url = 'https://raw.githubusercontent.com/akshayraj0674/MachineLearning/refs/heads/main/project-1-me-4127-e-2025-26/train.csv'
 test_url = 'https://raw.githubusercontent.com/akshayraj0674/MachineLearning/refs/heads/main/project-1-me-4127-e-2025-26/test.csv'
@@ -18,38 +18,28 @@ print(test.head())
 print(sample_submission.head())
 
 
-target_col = 'cost'
-id_col = 'id'
-
-features_cols = [col for col in train.columns if col not in [target_col, id_col]]
-
-
-x = train[features_cols]
-y = train[target_col]
-x_test = test[features_cols]
+x_train = train.drop('cost', axis=1)
+y_train = train['cost']
+x_test = test.copy()
 
 
-x = x.fillna(x.median())
-x_test = x_test.fillna(x_test.median())
+imputer = SimpleImputer(strategy='mean')
+x_train_imputed = imputer.fit_transform(x_train)
+x_test_imputed = imputer.transform(x_test)
 
 
-scaler = StandardScaler()
-x = scaler.fit_transform(x)
-x_test = scaler.transform(x_test)
+model = DecisionTreeRegressor(
+    max_depth=5,
+    min_samples_leaf=10,
+    min_samples_split=20,
+    random_state=0
+)
+model.fit(x_train_imputed, y_train)
 
 
-x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=42)
-
-
-model = HuberRegressor()
-model.fit(x_train, y_train)
-
-
-val_pred = model.predict(x_val)
-
-test_pred = model.predict(x_test)
+predictions = model.predict(x_test_imputed)
 
 
 submission = sample_submission.copy()
-submission[target_col] = test_pred
+submission['cost'] = predictions
 submission.to_csv('submission.csv', index=False)
