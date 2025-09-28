962372679357ecab3c5fbe00b444f7f74eed22f7
Akshay Raj P <akshay_b220666pe@nitc.ac.in>
2025-09-28 18:19:22 +0530

    learning

--- ML.py
+++ ML.py
@@ -28,9 +28,9 @@ x_test_imputed = imputer.transform(x_test)
 
 
 model = DecisionTreeRegressor(
-    max_depth=18,
-    min_samples_leaf=100,
-    min_samples_split=200,
+    max_depth=20,
+    min_samples_leaf=200,
+    min_samples_split=500,
     random_state=0
 )
 model.fit(x_train_imputed, y_train)
