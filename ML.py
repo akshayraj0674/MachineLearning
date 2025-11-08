d0f13ee84965bf03e0a994c1535033f66d32899c
Akshay Raj P <akshay_b220666pe@nitc.ac.in>
2025-11-08 17:10:46 +0530

    learning

--- ML.py
+++ ML.py
@@ -1,11 +1,2 @@
-pip install yfinance
-
-import yfinance as yf
-
-nifty = yf.Ticker("^NSEI")
-
-df = nifty.history(period="3y")
-
-print(df)
-
-df.to_csv("nift50_data.csv")
+impoit pandas as pd
+import matplotlib.pyplot as plt
